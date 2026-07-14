09625514536b6c73b2b2ed8824f64277cca95397
Philipp A. <flying-sheep@web.de>
2026-07-14 13:56:34 +0200

    fix: don’t crash when checking for numeric dtypes (#290)

--- src/anndata2ri/_conv.py
+++ src/anndata2ri/_conv.py
@@ -2,7 +2,6 @@ from __future__ import annotations
 
 from typing import TYPE_CHECKING
 
-import numpy as np
 import pandas as pd
 from rpy2.robjects import conversion, numpy2ri, pandas2ri
 
@@ -12,6 +11,7 @@ from . import scipy2ri
 if TYPE_CHECKING:
     from collections.abc import Callable
 
+    import numpy as np
     from rpy2.rinterface import Sexp
     from scipy.sparse import spmatrix
 
@@ -26,7 +26,7 @@ _mat_converter = numpy2ri.converter + scipy2ri.converter
 
 def mat_py2rpy(obj: np.ndarray | spmatrix | pd.DataFrame) -> Sexp:
     if isinstance(obj, pd.DataFrame):
-        numeric_cols = obj.dtypes.map(lambda dt: np.issubdtype(dt, np.number))
+        numeric_cols = obj.dtypes.map(pd.api.types.is_numeric_dtype)
         if not numeric_cols.all():
             non_num = numeric_cols.index[~numeric_cols]
             msg = f'DataFrame contains non-numeric columns {list(non_num)}'
